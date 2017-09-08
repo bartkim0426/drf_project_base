6e93091bf1cd0ae8bce75199e5909196ddc8c361
bartkim0426 <bartkim0426@gmail.com>
2017-09-08 16:37:07 +0900

    INIT : .gitignore
    Files:
    *
    
    Why:
    *
    
    ETC:
    *

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,21 +14,13 @@ MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 
 INTERNAL_IPS = ['127.0.0.1', ]
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["POSTGRES_DB_NAME"],
+        'USER': os.environ["POSTGRES_USER"],
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ["POSTGRES_DB_NAME"],
-#         'USER': os.environ["POSTGRES_USER"],
-#         'PASSWORD': os.environ["POSTGRES_PASSWORD"],
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
