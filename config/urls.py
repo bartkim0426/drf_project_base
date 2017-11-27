48e3875cf7c26a02b5f025117529cf66a672cfc5
bartkim0426 <bartkim0426@gmail.com>
2017-11-27 15:54:14 +0900

    ADD : django-rest-framework
    Files:
    *
    
    Why:
    *
    
    ETC:
    *

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
